b6e5db151d06dfb196e74850ee07d51093fca4c5
alexesp <aleizaes@hotmail.com>
2025-03-19 00:27:59 +0100

    Crear static resource

---
+++ CollectionView/Selectors/ProductDataTemplateSelector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CollectionView.MVVM.Models;
+
+namespace CollectionView.Selectors
+{
+    public class ProductDataTemplateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var product = item as Product;
+            if(!product.HasOffer)
+            {
+                Application
+                    .Current
+                    .Resources
+                    .TryGetValue("ProductStyle", out var productStyle);
+                return productStyle as DataTemplate;
+            }
+            return new DataTemplate();
+        }
+    }
+}
